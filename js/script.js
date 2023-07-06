ee997314f763d835ff9e6cd7e63b8fb8cafbc61b
Dhinakaran R <dhinakings123@outlook.com>
2023-07-06 16:42:45 +0530

    Tips Calculation Form

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//
+function calculation() {
+    let calc1 = Number(document.getElementById("fname").value);
+    let calc2 = Number(document.getElementById("lname").value);
+    let totalTipsRupee = (calc1 / 100) * calc2;
+    // 
+    let calc3 = document.getElementById("dob").value = totalTipsRupee;
+    let totalAmount = calc1 + calc3;
+    //
+    let calc4 = document.getElementById("email").value = totalAmount;
+};
+
